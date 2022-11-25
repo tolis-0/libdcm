2f103ab3bb7d809f713f88082ba770fa24400b7f
tolis-0 <yotravik@gmail.com>
2022-11-25 13:18:48 +0200

    unsigned

--- src/number_strings.c
+++ src/number_strings.c
@@ -2,37 +2,40 @@
 #include "dcmath.h"
 
 // for numbers up to 7 digits
-int is_permut(int a, int b){
+int is_permut(unsigned int a, unsigned int b){
 	return digit_id(a) == digit_id(b);
 }
 
-int are_permut(int a, int b, int c){
+int are_permut(unsigned int a, unsigned int b, unsigned int c){
 	int digit_id_a = digit_id(a);
 	if (digit_id_a != digit_id(b)) return 0;
 	return digit_id_a == digit_id(c);
 }
 
+
+
 // for numbers that can go above 7 digits
-int is_permutl(long a, long b){
+int is_permutl(unsigned long a, unsigned long b){
 	return digit_idl(a) == digit_idl(b);
 }
 
-int are_permutl(long a, long b, long c){
+int are_permutl(unsigned long a, unsigned long b, unsigned long c){
 	long digit_id_a = digit_idl(a);
 	if (digit_id_a != digit_idl(b)) return 0;
 	return digit_id_a == digit_idl(c);
 }
 
 
+
 // Calculating digit id
 // numbers that have the same digits but in different order will have the same digit id
-int digit_id(int n){
+int digit_id(unsigned int n){
 	long ret = 0;
 	while(n) ret += 1<<((n%10)*3), n /= 10;
 	return ret;
 }
 
-long digit_idl(long n){
+long digit_idl(unsigned long n){
 	long ret = 0;
 	while(n) ret += 1LL<<((n%10)*6), n /= 10;
 	return ret;
@@ -40,26 +43,31 @@ long digit_idl(long n){
 
 
 
-int is_palindrome(int num){
+int is_palindrome(unsigned int num){
     int n = num, rev = 0;
 	while (n) rev = rev*10 + (n%10), n /= 10;
 	return num == rev;
 }
 
-int is_palindromel(long num){
+int is_palindromel(unsigned long num){
 	long n = num, rev = 0;
 	while (n) rev = rev*10 + (n%10), n /= 10;
 	return num == rev;
 }
 
-int reverse(int num){
+
+
+int reverse(unsigned int num){
 	int n = num, rev = 0;
 	while (n) rev = rev*10 + (n%10), n /= 10;
 	return rev;
 }
 
-long reversel(long num){
+long reversel(unsigned long num){
 	long n = num, rev = 0;
 	while (n) rev = rev*10 + (n%10), n /= 10;
 	return rev;
 }
+
+
+
--- src/number_strings.h
+++ src/number_strings.h
@@ -1,19 +1,19 @@
 #ifndef NUMBER_STRINGS_H
 #define NUMBER_STRINGS_H
 
-int is_permut(int a, int b);
-int are_permut(int a, int b, int c);
+int is_permut(unsigned int a, unsigned int b);
+int are_permut(unsigned int a, unsigned int b, unsigned int c);
 
-int is_permutl(long a, long b);
-int are_permutl(long a, long b, long c);
+int is_permutl(unsigned long a, unsigned long b);
+int are_permutl(unsigned long a, unsigned long b, unsigned long c);
 
-int digit_id(int n);
-long digit_idl(long n);
+int digit_id(unsigned int n);
+long digit_idl(unsigned long n);
 
-int is_palindrome(int num);
-int is_palindromel(long num);
+int is_palindrome(unsigned int num);
+int is_palindromel(unsigned long num);
 
-int reverse(int num);
-long reversel(long num);
+int reverse(unsigned int num);
+long reversel(unsigned long num);
 
 #endif // NUMBER_STRINGS_H
