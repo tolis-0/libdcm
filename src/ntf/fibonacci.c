f25a57728917fec151e0d8253564a1f022563bfe
tolis-0 <yotravik@gmail.com>
2023-05-26 20:00:30 +0300

    added fibonacci

---
+++ src/ntf/fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdint.h>
+#include <math.h>
+#include "ntf.h"
+
+uint64_t digits_fib (uint64_t n) {
+	long double log10_fib;
+
+	if (n < 7) return 1L;
+	log10_fib = n*log10_phi - log10_sqrt5;
+
+	return (uint64_t) ceill(log10_fib);
+}
+
+uint32_t pisano (uint64_t n) {
+	uint64_t fib1, fib2, fib3;
+    uint32_t i;
+
+    fib1 = 0, fib2 = 1;
+
+	for (i = 1;; i++) {
+		fib3 = (fib1 + fib2) % n;
+		if (fib2 == 0 && fib3 == 1) break;
+		fib1 = fib2;
+		fib2 = fib3;
+    }
+
+    return i;
+}
+
+uint64_t fib_mod (uint64_t n, uint64_t modulo) {
+	
+	// TODO
+	
+	return 0;
+}
--- src/ntf/ntf.h
+++ src/ntf/ntf.h
@@ -19,4 +19,14 @@ int mobius_f (int N, int* isprime);
 void mobius_setup (int *mobius, int limit, int *primes, int primes_size);
 void rec_mob_setup(int *mobius, int limit, int num, int i, int *primes, int primes_size);
 
+
+/* ntf/fibonacci.c*/
+#define log10_sqrt5 	0.349485002168009402393130552637753487L
+#define log10_phi   	0.208987640249978733769272089237555417L
+
+uint64_t digits_fib (uint64_t n);
+uint32_t pisano (uint64_t n);
+uint64_t fib_mod (uint64_t n, uint64_t modulo);
+
+
 #endif // _NTF_H
