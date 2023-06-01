7809de654d46fa86296351697147fe7c596f0e6e
tolis-0 <yotravik@gmail.com>
2023-06-02 01:05:21 +0300

    bugfixes for ltbs

--- src/dcalgs/dcalgs.h
+++ src/dcalgs/dcalgs.h
@@ -14,14 +14,14 @@
 /* 	Same but if var <= x then condition is true, otherwise it's false */
 
 #define ltbs(var, start, end, condition) \
-	FT_AND_LT_BS(1, 0, var, start, end, condition)
+	FT_AND_LT_BS(1, 0, var, start, end, !(condition))
 
 #define FT_AND_LT_BS(is_lt, is_ft, var, start, end, condition) \
 	var = ((start) + (end))/2; \
 	for (int64_t var##_r = (start), var##_l = (end); \
-		(var##_r < var##_l && (var = (var##_r + var##_l)/2, 1)) \
+		(var##_r < var##_l && (var = (var##_r + var##_l)/2 + is_lt, 1)) \
 		|| ((var == var##_r) ? 0 : (var = var##_r, 1)); \
-		(condition) ? (var##_l = var + is_lt) : (var##_r = var + is_ft))
+		(condition) ? (var##_l = var - is_lt) : (var##_r = var + is_ft))
 
 /*	
  *	Example Usage:
